6005e26ebb23c4bb2d63d17bc8517d2ac234ac68
Felipe Dário <fala@felipedario.com>
2026-09-01 10:31:44 -0300

    fix: melhorias de performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import partytown from '@astrojs/partytown';
+
 export default defineConfig({
   output: 'static',
+
   build: {
     format: 'directory'
-  }
+  },
+
+  integrations: [partytown()]
 });
